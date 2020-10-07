8e8379605508f2205ca6750c1d3393b297d6958d
Vando dos reis <vandodosreis2001@gmail.com>
2020-10-07 16:30:28 -0300

    Realizando mais tratativas nos erros possiveis

--- js/main.js
+++ js/main.js
@@ -6,17 +6,28 @@ function calcular(tipo, valor) {
     }
 
     if (valor === '/' || valor === '*' || valor === '-' || valor === '+') {
+      let operador = document.getElementById('resultado').value
+      if(operador.includes('/') || operador.includes('*') || operador.includes('+')) {
+        return document.getElementById('resultado').value
+      }
       document.getElementById('resultado').value += valor
     }
 
     if (valor === '=') {
       var resultado_campo = eval(document.getElementById('resultado').value)
 
-      document.getElementById('resultado').value = resultado_campo
+      // caso o campo esteja vazio
+      if (resultado_campo == undefined) {
+        document.getElementById('resultado').value = 'Erro!'
+        alert('Digite dois número para poder realizar o calculo')
+        document.getElementById('resultado').value = ''
+      } else {
+        document.getElementById('resultado').value = resultado_campo
+      }
     }
-
+    
     if (valor === '.') {
-      document.getElementById('resultado').value += valor
+      document.getElementById('resultado').value += valor      
     }
       
   } else if (tipo === 'valor') {
